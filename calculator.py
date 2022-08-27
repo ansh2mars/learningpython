8ad8d3f0b97047f6313f9b5b1a98fae997cbcd37
ansh2mars <112288634+ansh2mars@users.noreply.github.com>
2022-08-27 16:36:24 -0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -101,3 +101,118 @@ def SQRT():
   SqRoNum = int(input("Enter the number you want to square root: "))
   res = math.sqrt(SqRoNum)
   print("The square root of " + str(SqRoNum) + " is " + str(res))
+
+    def CBRT():
+  print(Fore.BLACK + "You chose cube root")
+  CbRoNum = int(input("Enter the number you want to cube root: "))
+  res = round(CbRoNum**(1 / 3), 10)
+  print(str(CbRoNum) + " cube root is " + str(res))
+  
+
+def End():
+  print(Fore.LIGHTYELLOW_EX + "Thanks for using this calculator!")
+  sys.exit()
+
+
+def I():
+  try:
+    user_input = int(input(Fore.GREEN +" 1=Add,\n 2=Subtract,\n 3=Multiply,\n 4=Divide,\n 5=Powers,\n 6=Average,\n 7=Extend Add,\n 8=Extend Subtract,\n 9=Square,\n 10=Cube,\n 11=Square Root,\n 12=Cube Root,\n 13=End\n Choose: "))
+  except:
+    print( Fore.RED + "Invalid Input. Please try again...")
+    
+  if user_input == 1:
+    Add()
+    A = input("Thank you for trying addition. Want to try more? yes/no: ")
+    if A.lower() == "yes":
+      I()
+    elif A.lower() == "no":
+      End()
+  if user_input == 2:
+    Subtract()
+    S = input("Thank you for trying subtraction. Want to try more? yes/no: ")
+    if S.lower() == "yes":
+      I()
+    elif S.lower() == "no":
+      End()
+  if user_input == 3:
+    Multiply()
+    M = input("Thank you for trying multiplication. Want to try more?yes/no: ")
+    if M.lower() == "yes":
+      I()
+    elif M.lower() == "no":
+      End()
+  if user_input == 4:
+    Divide()
+    D = input("Thank you for trying division. Want to try more?yes/no:  ")
+    if D.lower() == "yes":
+      I()
+    elif D.lower() == "no":
+      End()
+  if user_input == 5:
+    Powers()
+    P = input("Thank you for trying powers. Want to try more?yes/no: ")
+    if P.lower() == "yes":
+      I()
+    elif P.lower() == "no":
+      End()
+  if user_input == 6:
+    Average()
+    Am = input("Thank you for trying average. Want to try more?yes/no: ")
+    if Am.lower() == "yes":
+      I()
+    elif Am.lower() == "no":
+      End()
+  if user_input == 7:
+    EAdd()
+    EA = input("Thank you for trying extended addition. Want to try more?yes/no: ")
+    if EA.lower() == "yes":
+      I()
+    elif EA.lower() == "no":
+      End()
+  if user_input == 8:
+    ESubtract()
+    ES = input("Thank you for trying extended subtraction. Want to try more?yes/no: ")
+    if ES.lower() == "yes":
+      I()
+    elif ES.lower() == "no":
+      End()
+  if user_input == 9:
+    S()
+    Squ = input("Thank you for trying square. Want to try more?yes/no: ")
+    if Squ.lower() == "yes":
+      I()
+    elif Squ.lower() == "no":
+      End()
+  if user_input == 10:
+    C()
+    Cub = input("Thank you for trying cube. Want to try more?yes/no: ")
+    if Cub.lower() == "yes":
+      I()
+    elif Cub.lower() == "no":
+      End()
+  if user_input == 11:
+    SQRT()
+    SR = input("Thank you for trying square root. Want to try more?yes/no: ")
+    if SR.lower() == "yes":
+      I()
+    elif SR.lower() == "no":
+      End()
+  if user_input == 12:
+    CBRT()
+    CR = input("Thank you for trying cube root. Want to try more?yes/no: ")
+    if CR.lower() == "yes":
+      I()
+    elif CR.lower() == "no":
+      End()
+  if user_input == 13:
+    End()
+  if user_input > 13 or user_input < 1:
+    print(Fore.RED + "Invalid Input. Please try again...")
+    I()
+
+
+def main():
+    I()
+
+if __name__ == "__main__":
+    main()
