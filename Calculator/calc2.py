897e7358c6eb35f89313bc516c898ba7ffee919b
Ansh Khandelwal <ansh2mars@gmail.com>
2024-12-26 17:26:12 +0530

    added Trig function and edited main

--- Calculator/calc2.py
+++ Calculator/calc2.py
@@ -185,6 +185,43 @@ def CTC():
     else:
         print(f"I'm sorry, but '{mmmr}' is an invalid input. Please try again.")
         CTC()
+def Trig(func, arc):
+    if arc:
+        def Main(nu, de):
+            print("Okay, we will continue with the sine.")
+            num = int(input("Enter the numerator of the fraction of your sine: "))
+            den = int(input("Enter the denominator of the fraction of your sine: "))
+            if nu and not de:
+                num = math.sqrt(num)
+            elif de and not nu:
+                den = math.sqrt(den)
+            elif nu and de:
+                num = math.sqrt(num)
+                den = math.sqrt(den)
+            if den == 0:
+                print("Error: Denominator cannot be zero.")
+                return
+            x = num / den
+            if not -1 <= x <= 1:
+                print(f"The fraction {num}/{den} or {x} is out of range for arcsine calculation. It must be between -1 and 1.")
+                return
+            mf = getattr(math, func)
+            rx = mf(x)
+            print(f"The arcsine in radians is about {round(rx, 10)} radians.")
+            dx = (rx * 180) / math.pi
+            print(f"The arcsine in degrees is about {round(dx, 5)}°.")
+        ndbo = input("Before we begin, is there square root in the sine? If so, in which of the following? (n for numerator/d for denominator/b for both/o for none): ")
+        if ndbo == 'n':
+            Main(True, False)
+        elif ndbo == 'd':
+            Main(False, True)
+        elif ndbo == 'b':
+            Main(True, True)
+        elif ndbo == 'o':
+            Main(False, False)
+        else:
+            print(f"Invalid input for square root choice.")
+
 
 def Main():
     print("\n1: Addition\n2: Subtraction\n3: Multiplication\n4: Division\n5: Exponents\n6: Factorial\n7: Roots\n8: Central Tendency\n9: Logarithms\n10: Trigonometry\n11: End")
